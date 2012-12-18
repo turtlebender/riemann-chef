8b61cb74eb6a56c2a2edf2710ff9585e4672484d
Kief Morris <kmorris@thoughtworks.com>
2012-12-18 13:58:28 +0000

    Fix what I broke

--- recipes/health.rb
+++ recipes/health.rb
@@ -4,12 +4,11 @@ gem_package 'riemann-tools' do
   notifies :restart, resources(:service => 'riemann-health')
 end
 
-runit_service 'riemann-health' do
-  options :host => node['riemann']['server']
-end
-
 service 'riemann-health' do
   supports :restart => true
   action [:start]
 end
 
+runit_service 'riemann-health' do
+  options :host => node['riemann']['server']
+end
